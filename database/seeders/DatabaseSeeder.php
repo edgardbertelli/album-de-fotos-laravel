97faf9291d96338ada5a538ef6fb09f470d4f736
Edgard Bertelli <bertelliedgard@gmail.com>
2023-04-25 15:30:25 -0300

    criacao de seeder de usuario para testes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('users')->insert([
+            'name' => 'Edgard Bertelli',
+            'email' => 'bertelliedgard@gmail.com',
+            'birth_date' => '1990-07-16',
+            'phone_number' => '(16) 99786-9410',
+            'status' => 'ativo',
+            'password' => Hash::make('meuteste123'),
+        ]);
     }
 }
